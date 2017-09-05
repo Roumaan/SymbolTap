5d1b3143ccb0f3c23569525aa2ed0de81e8f26d0
Roumaan <romanchik_2002@mail.ru>
2017-09-05 18:18:39 +0300

    Revert "git"
    This reverts commit b45576441fa628a4106e6ed112477ff0712f432c.

--- Assets/wakatime-unity-master/Editor/WakaTime/AssetChangedChecker.cs
+++ Assets/wakatime-unity-master/Editor/WakaTime/AssetChangedChecker.cs
@@ -8,7 +8,7 @@ namespace WakaTime {
 			foreach (var str in importedAssets) {
 				Main.OnAssetSaved (Main.GetProjectPath () + str);
 
-//				Debug.Log ("Reimported Asset: " + str);
+//		-		Debug.Log ("Reimported Asset: " + str);
 
 			}
 			foreach (var str in deletedAssets) {
